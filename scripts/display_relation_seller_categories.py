c9f131ddff420a48a43f9feb1c4970d3ed91bf16
smelezan <sebastien.melezan@etu.u-bordeaux.fr>
2020-12-16 23:58:34 +0100

    commit

---
+++ scripts/display_relation_seller_categories.py
@@ -0,0 +1,49 @@
+from tulip import tlp
+import csv
+
+
+def main(g):
+    category = graph['category']
+    viewLabel = graph['viewLabel']
+    full_info_vendors_path = 'C:\\Users\\sebas\\cours\\Master2\\Analyse-Reseau\\projetDarkWeb\\DarkWeb\\data\\full_info_vendors.csv'
+    # datasPath = "C:/Users/sebas/cours/Master2/Analyse-Reseau/projetDarkWeb/DarkWeb/data"
+    viewMetric = graph['viewMetric']
+    viewIcon = graph['viewIcon']
+    viewColor = graph['viewColor']
+    test = graph['test']
+    popularity = graph['popularity']
+    g.clear()
+    nodes = {}
+    counter = 0
+    with open(full_info_vendors_path, newline='', encoding='utf-8') as csvfile:
+        drops = csv.DictReader(csvfile, delimiter=',')
+        for row in drops:
+            product_subcategory = row['product_sub_category']
+            if len(product_subcategory) == 0:
+                continue
+            vendor = row['vendor']
+            node_product_subcategory = tlp.node()
+            node_vendor = tlp.node()
+            if product_subcategory in nodes:
+                node_product_subcategory = nodes[product_subcategory]
+            else:
+                node_product_subcategory = g.addNode()
+                viewIcon[node_product_subcategory] = "md-circle"
+                viewLabel[node_product_subcategory] = product_subcategory
+                nodes[product_subcategory] = node_product_subcategory
+                category[node_product_subcategory] = row['product_category']
+
+            if vendor in nodes:
+                node_vendor = nodes[vendor]
+            else:
+                node_vendor = g.addNode()
+                viewIcon[node_vendor] = "md-human"
+                viewLabel[node_vendor] = vendor
+                nodes[vendor] = node_vendor
+                popularity[node_vendor] = row['popularity'] == "True"
+
+            e = g.existEdge(node_product_subcategory, node_vendor, True)
+            if e.isValid():
+                viewMetric[e] += 1
+            else:
+                e = g.addEdge(node_product_subcategory, node_vendor)
---
+++ scripts/test.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import re
+famous_sellers = pd.read_csv(
+    'C:\\Users\\sebas\\cours\\Master2\\Analyse-Reseau\\projetDarkWeb\\DarkWeb\\data\\famousSellers.csv')
+sellers = pd.read_csv(
+    'C:\\Users\\sebas\\cours\\Master2\\Analyse-Reseau\\projetDarkWeb\\DarkWeb\\data\\sellers.csv')
+list_duplicates = pd.read_csv(
+    'C:\\Users\\sebas\\cours\\Master2\\Analyse-Reseau\\projetDarkWeb\\DarkWeb\\data\\list_duplicates_drop.csv',
+    delimiter=";")
+
+seller_duplicates = pd.read_csv(
+    'C:\\Users\\sebas\\cours\\Master2\\Analyse-Reseau\\projetDarkWeb\\DarkWeb\\data\\seller_duplicates_drop.csv', delimiter=";")
+
+
+def clean_sales_column(text):
+    clean = re.match('\\((.*)\\)', str(text))
+    return clean.groups()[0]
+
+
+def clean_vendor_trst_lvl_columns(text):
+    clean = re.match('(.*)(\\d)', str(text))
+    return clean.groups()[1]
+
+
+def clean_subcat_column(text):
+    clean = str(text).replace(' ', '')
+    clean = re.match('(Item#.\\d+-)(.+)(-.*)', str(clean))
+    clean = re.match('(.*)\\/(.*)', clean.groups()[1])
+    return clean.groups()[0]
+
+
+def clean_cat_column(text):
+    clean = text.strip()
+    clean = re.match('(\\W*)(.+)(\\s*)', str(clean))
+    return clean.groups()[1]
+
+
+def clean_usd_column(text):
+    return text.replace(',', '')
+
+
+def get_cat_subcat_vendors(dataframe):
+    colums_to_drop = ['product_url', 'product_title',
+                      'product_views_sales_quantityleft', 'vendor_url', 'BTC', 'file', 'crawling_date']
+    result = dataframe.dropna()
+    result = result.drop(columns=colums_to_drop, axis=1)
+    result['product_id_subcategory'] = result['product_id_subcategory'].apply(
+        clean_subcat_column)
+
+    result['product_category'] = result['product_category'].apply(
+        clean_cat_column)
+
+    products_sells = result.dropna()
+    products_sells['USD'] = products_sells['USD'].apply(clean_usd_column)
+    products_sells['USD'] = products_sells['USD'].astype(float)
+    products_sells = products_sells.groupby(
+        ['product_category', 'product_id_subcategory', 'vendor']).agg({'USD': ['sum']}).reset_index()
+    return products_sells
+
+
+def get_seller_popularity(dataframe):
+
+    colums_to_drop = ['fe_allowed', 'percent_posfb',
+                      'fb_1Pos', 'fb_6Pos', 'fb_12Pos', 'fb_1Neu', 'fb_6Neu', 'fb_12Neu',
+                      'fb_1Neg', 'fb_6Neg', 'fb_12Neg', 'last', 'fb_left',
+                      'spendings', 'pgp', 'file', 'crawling_date', 'disputes_orders']
+
+    sellers = dataframe.drop(columns=colums_to_drop, axis=1)
+    sellers = sellers.drop(columns=['description'], axis=1)
+    sellers = sellers.dropna(subset=['trust'])
+    sellers['member_since'] = pd.to_datetime(
+        sellers['member_since'], format='%B %d, %Y')
+    sellers['lvl'] = sellers['lvl'].apply(clean_vendor_trst_lvl_columns)
+    sellers['trust'] = sellers['trust'].apply(clean_vendor_trst_lvl_columns)
+    sellers['sales'] = sellers['sales'].apply(clean_sales_column)
+    sellers.sales = sellers.sales.astype(int)
+    sellers.lvl = sellers.lvl.astype(int)
+    sellers.trust = sellers.trust.astype(int)
+    sellers.vendor = sellers.vendor.astype(str)
+
+    print(sellers.head())
+    return sellers
+
+
+def set_popularity(row):
+    # print(row.vendor)
+    return
+
+
+def compute_popularity(dataframe):
+    dataframe['popularity'] = dataframe.apply(
+        lambda row: row.vendor in famous_sellers.values, axis=1)
+    dataframe = dataframe.sort_values(by='popularity', ascending=False)
+    return dataframe
+
+
+def main():
+    vendors_subcat = get_cat_subcat_vendors(list_duplicates)
+    vendors_info = get_seller_popularity(seller_duplicates)
+    full_info_vendors = vendors_subcat.merge(vendors_info, on='vendor')
+    full_info_vendors.columns = ['vendor', 'product_category',
+                                 'product_sub_category', 'vendors', 'total_usd', 'sales', 'lvl', 'trust', 'member_since']
+    full_info_vendors = full_info_vendors.dropna()
+    full_info_vendors = compute_popularity(
+        full_info_vendors.drop(columns=['vendors'], axis=1))
+
+    full_info_vendors.to_csv(
+        'C:\\Users\\sebas\\cours\\Master2\\Analyse-Reseau\\projetDarkWeb\\DarkWeb\\data\\full_info_vendors.csv')
+
+
+main()
