78f1f5f6c5841b93d999f21c3ffa66ac6f73a8dd
mduban <mathieu.duban@gmail.com>
2020-12-16 21:00:54 +0100

    script histogramm

---
+++ scripts/dataTreatement/displayTotalByCategories.py
@@ -0,0 +1,50 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import utility
+
+def formatTitle(path) :
+    file_name = path.split('/')
+    file_name_splitted = file_name[2].split('_')
+    title = file_name_splitted[0] + ' ' + file_name_splitted[1] 
+    return title
+
+def read(path) :
+    with open(path) as csvfile :
+        spamread = csv.reader(csvfile)
+
+        title = utility.formatTitle(path)
+        degree_rotation = 90
+        subplot_left = 0.2
+        subplot_right = 0.3
+
+        axeX = []
+        axeY = []
+        cpt = False
+
+        for row in spamread:
+            if row == [] or cpt == False :
+                cpt = True
+                continue
+
+            usd = round(float(row[1]),2)
+            axeX.append(row[0])
+            axeY.append(usd)
+
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(left = subplot_left, bottom = subplot_right)
+        plt.bar(axeX, axeY)
+        plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
+        plt.xticks(rotation = degree_rotation)
+        plt.title(title)  
+
+def main() :
+    total_path = 'data/totalCategories/'
+    files = utility.getFile(total_path)
+    
+    for treatedFile in files:
+        csvFile = total_path + treatedFile 
+        read(csvFile)
+    plt.show()
+
+main()      
---
+++ scripts/dataTreatement/displayTotalBySubCategories.py
@@ -0,0 +1,62 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import utility
+
+def read(path) :
+
+    with open(path) as csvfile :
+        spamread = csv.reader(csvfile)
+
+        title = utility.formatTitle(path)
+        drugs_category = 'Drugs & Chemicals'
+        degree_rotation = 90
+        subplot_left = 0.2
+        subplot_right = 0.3
+
+        axeX = []
+        axeY = []
+        axeXDrugs = []
+        axeYDrugs = []
+
+        cpt = False
+
+        for row in spamread:
+            if row == [] or cpt == False :
+                cpt = True
+                continue
+            
+            usd = round(float(row[2]),2)
+            if row[0] not in drugs_category: 
+                axeX.append(row[0])
+                axeY.append(usd)
+            else:
+                axeXDrugs.append(row[1])
+                axeYDrugs.append(usd)
+
+        # All sub categories without Drugs
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(left = subplot_left, bottom = subplot_right)
+        plt.bar(axeX, axeY)
+        plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
+        plt.xticks(rotation = degree_rotation) 
+        plt.title(title)
+
+        # Drugs sub categories
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(left = subplot_left, bottom = subplot_right)
+        plt.bar(axeXDrugs, axeYDrugs)
+        plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
+        plt.xticks(rotation = degree_rotation) 
+        plt.title(drugs_category)
+
+def main() :
+    total_path = 'data/totalSubCategories/'
+    files = utility.getFile(total_path)
+    
+    for treatedFile in files:
+        csvFile = total_path + treatedFile 
+        read(csvFile)
+    plt.show()
+
+main()      
